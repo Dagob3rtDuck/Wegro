cbc92c4cb06a2451e142fdc2266b2a0416a1b9ab
Mark Jansen <Markxjansen@gmail.com>
2017-11-21 15:23:43 +0100

    container

--- landing.php
+++ landing.php
@@ -2,9 +2,19 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Welkom Bij Wegro</title>
 </head>
 <body>
-
+<div class="container">
+<div class="col">
+    metselbedrijf
+</div>
+<div class="col">
+   Bouwbedrijf
+</div>
+<div class="col">
+    Tekenbureau
+</div>
+</div>
 </body>
 </html>
